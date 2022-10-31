b170b94b8f56274dc91304ac57bfe21682a2b69a
Joel Edem Amenuvor <jaamen5@gmail.com>
2022-10-31 12:18:59 +0000

    added register and login pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./app.scss";
 import Home from "./pages/home/Home";
+import Register from "./pages/register/Register";
 import Watch from "./pages/watch/Watch";
+import Login from "./pages/login/Login";
 
 function App() {
   return (
     <div className="App">
-      <Watch />
+      <Home />
     </div>
   );
 }
